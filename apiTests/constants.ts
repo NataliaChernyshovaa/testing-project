c35d9bf3b0fe6a1f89880797f277293b70e9e2f5
NataliaChernyshovaa <natcernysova@gmail.com>
2023-12-26 16:46:41 +0100

    feat: api tests

---
+++ apiTests/constants.ts
@@ -0,0 +1,13 @@
+export const URL: string = 'https://reqres.in/api'
+export const userData = {
+        name: 'morpheus',
+        job: 'leader'
+}
+
+export const userName = {
+    name: 'zephyr'
+}
+export const registrationData = { email : 'sydney@fife' }
+
+export const usersQuantity = 6
+export const randomUser = Math.floor(Math.random() * usersQuantity) + 1
